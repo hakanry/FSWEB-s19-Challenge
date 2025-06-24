129482e6c30c9f96a8691fb6b9d735da474fea16
Hakan Reşit YALÇIN <hakanryalcin@gmail.com>
2025-06-24 14:52:05 +0300

    project created and package design added

---
+++ src/main/java/com/workintech/s19_twitter_challange/S19TwitterChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.s19_twitter_challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S19TwitterChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S19TwitterChallangeApplication.class, args);
+	}
+
+}
